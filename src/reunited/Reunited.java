464f66de26690c384097c443451f2c36d6776b05
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2024-02-23 18:13:36 +0700

    project file

--- src/reunited/Reunited.java
+++ src/reunited/Reunited.java
@@ -4,6 +4,8 @@ import mindustry.mod.*;
 import reunited.gen.*;
 
 public class Reunited extends Mod{
+    public static final boolean DEBUGGING_SPRITE = false;
+    public static String modname = "reunited";
     @Override
     public void loadContent(){
         EntityRegistry.register();
