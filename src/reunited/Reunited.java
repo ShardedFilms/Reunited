4bdd7e7fd9876f8e43dbf3921ab81ba363c8f767
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2024-02-25 10:19:31 +0700

    Gen2

--- src/reunited/Reunited.java
+++ src/reunited/Reunited.java
@@ -1,16 +1,32 @@
 package reunited;
 
+import arc.Core;
+import mindustry.*;
 import mindustry.mod.*;
 import reunited.content.*;
 import reunited.gen.*;
+import reunited.tools.UnityPixmaps;
 
 public class Reunited extends Mod{
     public static final boolean DEBUGGING_SPRITE = true;
     public static String modname = "reunited";
     public static String author = "sharded_dev";
+
+
     @Override
     public void loadContent(){
         EntityRegistry.register();
         UUnitTypes.load();
+    }
+
+    public void Generate() {
+            Vars.content.units().each(u -> {
+                if(u.name.contains(Reunited.modname)){
+                    UnityPixmaps.saveUnitPixmap(Core.atlas.getPixmap(u.fullIcon).crop(), u);
+                }
+            });
+
+            UnityPixmaps.saveAddProcessed();
+
     }
 }
